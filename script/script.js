9859c2c410148ed8973c722265b57f71f26a0def
Asiye Koca <ak61@hotmail.nl>
2016-11-03 19:13:38 +0100

    Transavia Verhalen

--- script/script.js
+++ script/script.js
@@ -1,18 +1,20 @@
+// Filter button
+
 var button = document.querySelector('#button');
 var content = document.querySelector('#content');
 
-//    Als javascript niet beschikbaar is wordt de content dus NIET verborgen.
 content.classList.add('hidden');
 
-//    Wanneer er op de button geklikt wordt voeren we een functie uit.
 button.addEventListener("click", function () {
 
-    //        We schakelen de class .hidden
     content.classList.toggle('hidden');
 
 });
 
+// Toevoegen leeslijst
+
 // bron: http://stackoverflow.com/questions/22119673/find-the-closest-ancestor-element-that-has-a-specific-class
+
 function findAncestor (el, cls) {
     while ((el = el.parentElement) && !el.classList.contains(cls));
     return el;
@@ -21,13 +23,17 @@ function findAncestor (el, cls) {
 
 //regel eronder, selecteert alle elemeneten met .leeslijst-actie en stopt in array
 var leeslijstbutton = document.querySelectorAll('.leeslijst-actie');
+
 //regel eronder is loop 
 for (i = 0; i < leeslijstbutton.length; i++) {
+    
     //voor elke element voegt die click functie toe, leeslijstbutton is button van leeslijst
     leeslijstbutton[i].addEventListener("click", function(){
-        //findAncestor vind dichtsbijzijnde parent met class verhaalblokje
-        var verhaalblokje = findAncestor(this, 'verhaalblokje');
-        //Het gevonden verhaalblokje krijgt class animation
-        verhaalblokje.classList.add('animation');
-    });
+        
+    //findAncestor vind dichtsbijzijnde parent met class verhaalblokje
+    var verhaalblokje = findAncestor(this, 'verhaalblokje');
+        
+    //Het gevonden verhaalblokje krijgt class animation
+    verhaalblokje.classList.add('animation');
+});
 }
